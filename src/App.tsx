2dd00edc1f29c57cda252948cff6454701c020e2
marcellopz <marcellopz@hotmail.com>
2024-07-02 22:09:12 -0300

    theme and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { createTheme } from "@mui/material";
+import AppRoutes from "./navigation/routes/routes";
+import { ThemeProvider } from "@emotion/react";
+import { themeOptions } from "./theme";
+
+export const AppName = "Guild";
+
+const theme = createTheme(themeOptions);
+
 function App() {
   return (
-    <>
-      <div>xd</div>
-    </>
+    <ThemeProvider theme={theme}>
+      <AppRoutes />
+    </ThemeProvider>
   );
 }
 
--- src/navigation/routes/routes.tsx
+++ src/navigation/routes/routes.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PageNotFound from "./PageNotFound";
+import Navbar from "../../components/navbar/Navbar";
 
 const About = () => <h1>About</h1>;
 
@@ -6,8 +8,10 @@ export default function AppRoutes() {
   // navbar provider ?
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/about" element={<About />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
